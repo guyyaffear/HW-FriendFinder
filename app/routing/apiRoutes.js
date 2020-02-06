94ae3b14b3f398e862ce82b9e7160d58a0d61d59
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-02-05 22:37:53 -0500

    finishHw

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 var friends = require("../data/friends");
 
 module.exports = function (app) {
+    console.log(friends);
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
